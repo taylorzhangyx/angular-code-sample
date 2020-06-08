908ac10a1f4ff08f83525d3dadebd1508e2a2251
YuxinZhang <taylorzhangyx@gmail.com>
2020-06-08 16:55:11 -0700

    add hero details, component and navigation
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- angular-hero-app/src/app/app.module.ts
+++ angular-hero-app/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
